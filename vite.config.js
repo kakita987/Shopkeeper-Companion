ee9b4b8059ea78ec895ef2e5ba2e64da63a0d300
kakita987 <31381267+kakita987@users.noreply.github.com>
2026-07-30 21:35:52 -0600

    testing privacy link and google permissions

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+      },
+    },
+  },
   plugins: [
     {
       name: 'spreadsheet-proxy',
